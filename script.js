d58df28b9bf36511d82d6328ae5e3d65caa92a1d
mattailor007 <matthias.schneider89@gmail.com>
2022-02-06 21:50:13 +0100

    input check

--- script.js
+++ script.js
@@ -6,20 +6,30 @@ const RESULT_YEAR_TEXT = "in the year";
 function compute()
 {
     var deposit = document.getElementById("deposit").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
 
-    var gains = (deposit * (rate/100)) * years;
+    if (deposit > 0) {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
 
-    var yearInFuture = new Date().getFullYear() + parseInt(years);
+        var gains = (deposit * (rate/100)) * years;
 
-    document.getElementById('output-deposit').innerHTML
-        = RESULT_DEPOSIT_TEXT + " " + "<span style='background-color: yellow;'>" + deposit + "</span>";
-    document.getElementById('output-rate').innerHTML
-        = RESULT_RATE_TEXT + " " + "<span style='background-color: yellow;'>" + rate + "%" + "</span>";
-    document.getElementById('output-amount').innerHTML
-        = RESULT_AMOUNT_TEXT + " " + "<span style='background-color: yellow;'>" + gains + "</span>";
-    document.getElementById('output-year').innerHTML 
-        = RESULT_YEAR_TEXT + " " + "<span style='background-color: yellow;'>" + yearInFuture + "</span>";
+        var yearInFuture = new Date().getFullYear() + parseInt(years);
+
+        document.getElementById('output-deposit').innerHTML
+            = RESULT_DEPOSIT_TEXT + " " + "<span style='background-color: yellow;'>" + deposit + "</span>";
+        document.getElementById('output-rate').innerHTML
+            = RESULT_RATE_TEXT + " " + "<span style='background-color: yellow;'>" + rate + "%" + "</span>";
+        document.getElementById('output-amount').innerHTML
+            = RESULT_AMOUNT_TEXT + " " + "<span style='background-color: yellow;'>" + gains + "</span>";
+        document.getElementById('output-year').innerHTML 
+            = RESULT_YEAR_TEXT + " " + "<span style='background-color: yellow;'>" + yearInFuture + "</span>";
+    } else {
+        alert("Please enter a positive value!");
+        depositInput = document.getElementById("deposit");
+        depositInput.focus();
+        depositInput.select();
+    }
+
+        
 }
         
